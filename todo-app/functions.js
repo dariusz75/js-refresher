d891864d035afa2ce96dec5528a919e5304f3c30
Dariusz Franczak <darius32@op.pl>
2020-09-09 16:41:40 +0100

    complex dom rendering

--- todo-app/functions.js
+++ todo-app/functions.js
@@ -14,7 +14,7 @@ const saveToodos = function () {
   localStorage.setItem("todos", JSON.stringify(todos));
 };
 
-//Render todos on the screen
+//Render todos on the screen based on filters
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
     if (filters.hideCompleted === true && todo.completed === false) {
@@ -39,8 +39,24 @@ const renderTodos = function (todos, filters) {
   document.querySelector("#todos").appendChild(summary);
 
   filteredTodos.forEach(function (filteredTodo) {
-    const p = document.createElement("p");
-    p.textContent = filteredTodo.title;
-    document.querySelector("#todos").appendChild(p);
+    const todoEl = document.createElement("div");
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+
+    const textEl = document.createElement("span");
+    textEl.textContent = filteredTodo.title;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "x";
+
+    todoEl.appendChild(checkbox);
+    todoEl.appendChild(textEl);
+    todoEl.appendChild(removeButton);
+
+    document.querySelector("#todos").appendChild(todoEl);
   });
 };
+
+//Get the DOM elements for an individual todo
+const generateTodoDOM = function () {};
